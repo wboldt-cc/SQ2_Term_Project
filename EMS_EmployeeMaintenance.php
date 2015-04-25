1a5ff0df46a583f7024b1fc6b68568a590b91509
webshark9 <mjthiessen-cc@2A314-B04.conestogac.on.ca>
2015-04-24 21:23:22 -0400

    CRAWLING IN MY SKIN

--- EMS_EmployeeMaintenance.php
+++ EMS_EmployeeMaintenance.php
@@ -1,11 +1,12 @@
-<!---
-Filename: "Home Page.html"
-Programmer: William Boldt
-Date: December 8, 2013
---->
+<?php
+/// File: EMS_EmployeeMaintenance.php
+/// Project: EMS-PSS Term Project
+/// Programmers: Matthew Thiessen, Willi Boldt, Ping Chang Ueng, and Tylor McLaughlin
+/// First Version: April.21/2015
+/// Description: This page contains the fields required to edit or enter an
+///				 employee which will then be committed to the employee database
+?>
 <html>
-
-
 	<head>
 		
 		<style>
@@ -55,12 +56,14 @@ Date: December 8, 2013
 			$queryType = "insert";
 			$saveButtonValue = "Save";
 			
+			//saves user input prior to the post
 			if(isset($_POST['employeeTypeDropdown']))
 			{
-				$employeeType = $_POST['employeeTypeDropdown'];
-				$company = $_POST['company'];
+				$employeeType = $_POST['employeeTypeDropdown'];//gets the employee type
+				$company = $_POST['company'];//company the employee works at
 				
-				
+				//fulltime employee, fills out the fulltime
+				//employee feilds
 				if($employeeType == "ftEmployee")
 				{
 					$firstName = $_POST['firstName'];
@@ -71,7 +74,8 @@ Date: December 8, 2013
 					$salary = $_POST['salary'];
 					$reasonForTermination = $_POST['reasonForTermination'];
 				}
-				
+				//parttime employee, fills out the parttime
+				//employee feilds
 				if($employeeType == "ptEmployee")
 				{
 					$firstName = $_POST['firstName'];
@@ -82,7 +86,8 @@ Date: December 8, 2013
 					$hourlyRate = $_POST['hourlyRate'];
 					$reasonForTermination = $_POST['reasonForTermination'];
 				}
-				
+				//seasonal employee, fills out the seasonal
+				//employee feilds
 				if($employeeType == "sEmployee")
 				{
 					$firstName = $_POST['firstName'];
@@ -93,7 +98,8 @@ Date: December 8, 2013
 					$piecePay = $_POST['piecePay'];	
 					$reasonForTermination = $_POST['reasonForTermination'];
 				}
-				
+				//contract employee, fills out the contract
+				//employee feilds
 				if($employeeType == "cEmployee")
 				{
 					$dateOfIncorportation = $_POST['dob'];
@@ -103,12 +109,6 @@ Date: December 8, 2013
 					$contractAmount = $_POST['contractPay'];
 					$reasonForTermination = $_POST['reasonForTermination'];
 				}
-				//$contractCompanyName = "";
-				//$status = $_POST[''];
-				//$company = "";
-				//$employeeType = "";
-				//$employedWithCompany = $_POST[''];
-				//$reasonForTermination = $_POST[''];
 			}
 			/* check if the search employee page sent us the information we need to display an employee */
 			else if((isset($_SESSION['SINfromSearch'])) && ($_SESSION['SINfromSearch'] != "") &&
@@ -126,7 +126,6 @@ Date: December 8, 2013
 				$_SESSION['CompanyFromSearch'] = "";// reset the session variable
 				
 				$employeeType = $_SESSION['EmployeeTypeFromSearch'];
-				//$_SESSION['EmployeeTypeFromSearch'] = "";// reset the session variable
 				
 				$link = mysqli_connect($serverName, $userName, $password, $databaseName);// connect to the database
 			
@@ -155,7 +154,7 @@ Date: December 8, 2013
 
 						$queryString .= "WHERE SIN='$SIN' && Company=\"$company\";";
 					
-					
+						//gets info for the the employee that needs to be edited
 						if($result = $link->query($queryString))
 						{
 							
@@ -163,10 +162,7 @@ Date: December 8, 2013
 							{
 								$firstName = $row['First_Name'];
 								$lastName = $row['Last_Name'];
-								//SIN: " . $row['SIN'] . "</br>
 								$dateOfBirth = $row['Date_of_Birth'];
-								
-								//Employed with Company: " . $row['Company'] . " </br>";
 								
 								switch($employeeType)
 								{
@@ -209,7 +205,6 @@ Date: December 8, 2013
 						else// query failed
 						{
 							echo "Could not display the Employees Information. Sorry for the inconvenience";
-							//echo "<hr>$queryString";
 						}																
 								
 					
@@ -218,19 +213,19 @@ Date: December 8, 2013
 					{
 						$queryString .= "CT_Display WHERE Business_Number='$SIN' && Company=\"$company\";";
 						
+						//will only let an administrator view contract employee
 						if($userType != "administrator")
 						{
 							echo "<br>You do not have access to Contract Employees.<br>";
 						}
 						else
 						{
-									
+								
 							if($result = $link->query($queryString))
 							{
 								
 								while($row = $result->fetch_assoc())
 								{
-									//$contractCompanyName = $row['Contract_company_name'];
 									$contractCompanyName = $row['Company'];
 									$businessNumber  = $row['Business_Number'];
 									$dateOfIncorportation = $row['Date_of_incorportation'];
@@ -238,7 +233,6 @@ Date: December 8, 2013
 									$contractEndDate = $row['Contract_end_date'];
 									$reasonForTermination = $row['Reason_for_termination'];
 									$contractAmount = $row['Contract_amount'];
-									//Company: " . $row['Company'] . " </br>
 									$status = $row['Status'];											
 										
 								}
@@ -257,7 +251,6 @@ Date: December 8, 2013
 					
 					$link->close();		
 				}
-//echo "$SIN $company $employeeType";
 			}
 			else
 			{
@@ -281,7 +274,9 @@ Date: December 8, 2013
 		
 		
 		
-
+		<!--
+		Creates the navigation menu
+		-->
 		<div class="menu">
 			</br> <b>Operation Modes:</b> </br></br>
 			<a href= "EMS_HomePage.php" >Home</a><br></br>
@@ -294,7 +289,9 @@ Date: December 8, 2013
 
 		<div class="margin">
 		</div>
-
+		<!--
+		Creates the edit menu
+		-->
 		<div class="content"> </br>
 			<h2>Employee Maintenance</h2>
 			
@@ -319,7 +316,10 @@ Date: December 8, 2013
 		<div class="footer">
 			Copyright &copy MATTHEWSOFT
 		</div>
-		
+		<!--
+		Dynamically sets the fields to match the employee
+		that is being edited
+		-->
 		<script>
 			function employeeFormChange()
 			{		
@@ -531,62 +531,67 @@ Date: December 8, 2013
 			}
 			else// we have a connection
 			{
+				//checks to see what employee type is being edited/created
+				//will validate each field and submit to the database
+				//only if it is all valid
 				if(isset($_POST['employeeTypeDropdown']))
 				{
 					$queryType = $_POST['queryType'];
 					$validEntry = 0;
 					
+					//validate a fulltime employee
 					if($_POST['employeeTypeDropdown'] == 'ftEmployee')
-					{			
+					{	
+						//validates first name
 						if(ValidateName($firstName, $errorMessage) == 1)
 						{
 							echo "First Name</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates last name
 						if(ValidateName($lastName, $errorMessage) == 1)
 						{
 							echo "Last Name</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates SIN
 						if(ValidateSocialInsuranceNumber($SIN, $errorMessage) == 1)
 						{
 							echo "Social Insurance Number</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates date of birth
 						if(ValidateDateOfBirth($dateOfBirth, $dateOfHire, $dateOfTermination, $errorMessage) == 1)
 						{
 							echo "Date Of Birth</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates date of birth
 						if(ValidateDateOfHire($dateOfBirth, $dateOfHire, $dateOfTermination, $errorMessage))
 						{
 							echo "Date Of Hire</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates date of termination
 						if(ValidateDateOfTermination($dateOfBirth, $dateOfHire, $dateOfTermination, $errorMessage))
 						{
 							echo "Date Of Termination</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates salary
 						if(ValidateSalary($salary, $errorMessage))
 						{
 							echo "Salary</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//creates the insert query if this is a new employee
 						if($queryType == "insert")
 						{
 							$insertQuery = "INSERT INTO Person (p_firstname, p_lastname, si_number, date_of_birth)";
@@ -596,7 +601,8 @@ Date: December 8, 2013
 							$insertQuery .= " VALUES (LAST_INSERT_ID(), LAST_INSERT_ID());";
 
 							$insertQuery .= "INSERT INTO fulltime_employee";
-							
+							//checks to see if date of termination was included, otherwise
+							//send a query without the date of termination
 							if($dateOfTermination != "")
 							{
 								$insertQuery .= " VALUES (LAST_INSERT_ID(), (SELECT companyID FROM Company WHERE companyName = \"". $company ."\"), '". $dateOfHire ."', '". $dateOfTermination ."', '". $reasonForTermination ."', ". $salary .", (SELECT status_id FROM Employee_Status WHERE status_type = 'Active'));";
@@ -605,7 +611,7 @@ Date: December 8, 2013
 							{
 								$insertQuery .= " VALUES (LAST_INSERT_ID(), (SELECT companyID FROM Company WHERE companyName = \"". $company ."\"), '". $dateOfHire ."', null, '". $reasonForTermination ."', ". $salary .", (SELECT status_id FROM Employee_Status WHERE status_type = 'Active'));";
 							}
-						}
+						}//if an employee is being updated, update the specified feild
 						else if($queryType == "update")
 						{
 							$insertQuery = "UPDATE Person
@@ -627,63 +633,64 @@ Date: December 8, 2013
 						{
 							$queryResult = $link->multi_query($insertQuery);
 						}
-						
+						//checks to see if the query was successful
 						if(!$queryResult)
 						{
 							echo "Could Not Add Employee";
 						}
-					}
+					}//validate a parttime employee if it was specified
 					else if($_POST['employeeTypeDropdown'] == 'ptEmployee')
 					{
+						//validates first name
 						if(ValidateName($firstName, $errorMessage) == 1)
 						{
 							echo "First Name</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates lastname
 						if(ValidateName($lastName, $errorMessage) == 1)
 						{
 							echo "Last Name</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates SIN
 						if(ValidateSocialInsuranceNumber($SIN, $errorMessage) == 1)
 						{
 							echo "Social Insurance Number</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates date of birth
 						if(ValidateDateOfBirth($dateOfBirth, $dateOfHire, $dateOfTermination, $errorMessage) == 1)
 						{
 							echo "Date Of Birth</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates date of hire
 						if(ValidateDateOfHire($dateOfBirth, $dateOfHire, $dateOfTermination, $errorMessage))
 						{
 							echo "Date Of Hire</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates date of termination
 						if(ValidateDateOfTermination($dateOfBirth, $dateOfHire, $dateOfTermination, $errorMessage))
 						{
 							echo "Date Of Termination</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validate salary
 						if(ValidateSalary($hourlyRate, $errorMessage))
 						{
 							echo "Hourly Rate</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//if the employee is created, use an insert statement
 						if($queryType == "insert")
 						{
 							$insertQuery = "INSERT INTO Person (p_firstname, p_lastname, si_number, date_of_birth)";
@@ -692,7 +699,10 @@ Date: December 8, 2013
 							$insertQuery .= "INSERT INTO Employee (emp_id, person_id)";
 							$insertQuery .= " VALUES (LAST_INSERT_ID(), LAST_INSERT_ID());";
 
-						$insertQuery .= "INSERT INTO parttime_employee";
+							$insertQuery .= "INSERT INTO parttime_employee";
+							
+							//checks to see if the date of termination was included
+							//otherwise do not include in the statement
 							if($dateOfTermination != "")
 							{
 								$insertQuery .= " VALUES (LAST_INSERT_ID(), (SELECT companyID FROM Company WHERE companyName = \"". $company ."\"), '". $dateOfHire ."', '". $dateOfTermination ."', '". $reasonForTermination ."', ". $hourlyRate .", (SELECT status_id FROM Employee_Status WHERE status_type = 'Active'));";
@@ -701,7 +711,7 @@ Date: December 8, 2013
 							{
 								$insertQuery .= " VALUES (LAST_INSERT_ID(), (SELECT companyID FROM Company WHERE companyName = \"". $company ."\"), '". $dateOfHire ."', null, '". $reasonForTermination ."', ". $hourlyRate .", (SELECT status_id FROM Employee_Status WHERE status_type = 'Active'));";
 							}
-						}
+						}//if the employee is being edited, us an update statement 
 						else if($queryType == "update")
 						{
 							$insertQuery = "UPDATE Person
@@ -724,51 +734,51 @@ Date: December 8, 2013
 						{
 							$queryResult = $link->multi_query($insertQuery);
 						}
-						
+						//checks to see if the query passed or failed
 						if(!$queryResult)
 						{
 							echo "Could Not Add Employee";
 						}
-					}
+					}//validates a seasonal employee if that is what is selected
 					else if($_POST['employeeTypeDropdown'] == 'sEmployee')
 					{
-						
+						//validates first name
 						if(ValidateName($firstName, $errorMessage) == 1)
 						{
 							echo "First Name</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates last name
 						if(ValidateName($lastName, $errorMessage) == 1)
 						{
 							echo "Last Name</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates SIN
 						if(ValidateSocialInsuranceNumber($SIN, $errorMessage) == 1)
 						{
 							echo "Social Insurance Number</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates season
 						if(ValidateSeason($season, $errorMessage))
 						{
 							echo "Season</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates piece pay
 						if(ValidatePiecePay($piecePay, $errorMessage))
 						{
 							echo "Piece Pay</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
-						
+						}//if the employee is being created use
+						//an insert statement
 						if($queryType == "insert")
 						{
 							$insertQuery = "INSERT INTO Person (p_firstname, p_lastname, si_number, date_of_birth)";
@@ -779,7 +789,8 @@ Date: December 8, 2013
 							
 							$insertQuery .= "INSERT INTO seasonal_employee";
 							$insertQuery .= " VALUES (LAST_INSERT_ID(), (SELECT companyID FROM Company WHERE companyName = \"". $company ."\"), '". $season ."', ". $year .", ". $piecePay .", '". $reasonForTermination ."', (SELECT status_id FROM Employee_Status WHERE status_type = 'Active'));";
-						}
+						}//if the employee is being created
+						//use an update statement 
 						else if($queryType == "update")
 						{
 							$insertQuery = "UPDATE Person
@@ -801,56 +812,50 @@ Date: December 8, 2013
 						{
 							$queryResult = $link->multi_query($insertQuery);
 						}
-						
+						//checks to ensure the query succeeds
 						if(!$queryResult)
 						{
 							echo "Could Not Add Employee";
 						}
-					}
+					}//validates a contract employee if that selected
 					else if($_POST['employeeTypeDropdown'] == 'cEmployee')
 					{
+						//validates first name
 						if(ValidateName($firstName, $errorMessage) == 1)
 						{
 							echo "First Name</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates last name
 						if(ValidateName($lastName, $errorMessage) == 1)
 						{
 							echo "Last Name</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
-						/*if(ValidateBusinessNumber($businessNumber, $dateOfIncorportation, $errorMessage) == 1)
-						{
-							echo "Business Number</br>";
-							echo $errorMessage;
-							echo "</br></br>";
-							$validEntry = 1;
-						}*/
+						}//validates date of creation
 						if(ValidateDateOfCreation($businessNumber, $dateOfIncorportation, $errorMessage) == 1)
 						{
 							echo "Date Of Creation</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates contract start date
 						if(ValidateContractStartDate($dateOfIncorportation, $contractStartDate, $contractEndDate, $errorMessage) == 1)
 						{
 							echo "Contract Start Date</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates contract stop date
 						if(ValidateContractStopDate($dateOfIncorportation, $contractStartDate, $contractEndDate, $errorMessage) == 1)
 						{
 							echo "Contract Stop Date</br>";
 							echo $errorMessage;
 							echo "</br></br>";
 							$validEntry = 1;
-						}
+						}//validates fixed contract amount
 						if(ValidateFixedContractAmount($contractAmount, $errorMessage) == 1)
 						{
 							echo "Contract Amount</br>";
@@ -858,7 +863,8 @@ Date: December 8, 2013
 							echo "</br></br>";
 							$validEntry = 1;
 						}
-						
+						//if the employee is being created
+						//use an insert statement
 						if($queryType == "insert")
 						{
 							$insertQuery = "INSERT INTO Person (p_firstname, p_lastname, si_number, date_of_birth)";
@@ -870,6 +876,8 @@ Date: December 8, 2013
 							$insertQuery .= "INSERT INTO Contract_Employee";
 							$insertQuery .= " VALUES (LAST_INSERT_ID(), (SELECT companyID FROM Company WHERE companyName = \"". $company ."\"), '". $contractStartDate ."', '". $contractEndDate ."', ". $contractAmount .",'". $reasonForTermination ."', (SELECT status_id FROM Employee_Status WHERE status_type = 'Active'));";
 						}
+						//if the employee is being edited
+						//use an insert statement
 						else if($queryType == "update")
 						{
 							$insertQuery = "UPDATE Person
@@ -892,15 +900,14 @@ Date: December 8, 2013
 						{
 							$queryResult = $link->multi_query($insertQuery);
 						}
-						
+						//checks to see if the query failed
 						if(!$queryResult)
 						{
 							echo "Could Not Add Employee";
 						}
 					}
 				}
-			}
-			
+			}//close the link
 			$link->close();	
 		?>
 	</body>
